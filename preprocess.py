d677ed09689c2dce90704ac6641b64eb4855d4c7
Adam <twinbanj@gmail.com>
2020-01-20 21:49:22 +0000

    Changed data file urls

---
+++ preprocess.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def clean_urn(urn_series : pd.Series) -> pd.Series:
+
+    urn_series = urn_series.astype(str)
+    #strip the unwanted junk characters in the URN column
+    urn_series = urn_series.map(lambda x: x.lstrip('b'').rstrip('''))
+    urn_series = urn_series.str.replace("'", "")
+
+    return urn_series
