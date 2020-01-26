452b07d87b4e04b7396b3006c85f1666a1f7743c
twinbanj <twinbanj@gmail.com>
2020-01-26 21:20:41 +0000

    Added the import scripts for 2018 and 2017. Need to get the data

---
+++ skiptondata.py
@@ -0,0 +1,5 @@
+def get_skipsch_data(year : int) -> pd.DataFrame:
+school_data = get_school_data(year)
+skipsch = merge.loc[school_data['URN'].isin(["136664","141179","121716","121690"])] # SAM - can use ".isin()" method to replace all those OR statements you had before
+
+return skipsch
