f3ea286d45f9c0c9c079ea81d5a85209802a97b9
twinbanj <twinbanj@gmail.com>
2020-01-28 10:45:26 +0000

    Appended the data together into one cross year table

--- School_Comparison_code.py
+++ School_Comparison_code.py
@@ -7,4 +7,6 @@ data2018 = sk.get_skipsch_data(2018)
 data2019 = sk.get_skipsch_data(2019)
 data2017 = sk.get_skipsch_data(2017)
 
-print(data2017, data2018, data2019)
+skipschools = data2019.append([data2018, data2017], ignore_index= True, sort= False)
+
+print(skipschools)
